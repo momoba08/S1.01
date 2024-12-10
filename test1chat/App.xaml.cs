866727a9da39c559491f4f7e7e5f6002a9332e38
bamou <bamou@D360-S10>
2024-12-10 15:26:58 +0100

    Ajoutez des fichiers projet.

---
+++ test1chat/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace test1chat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ test1chat/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace test1chat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double trainSpeed = 5;
+        private double obsSpeed = 5;
+        private int score = 0;
+        private DispatcherTimer gameTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+        
+        private void StartGame()
+        {
+            score = 0;
+            trainSpeed = 5;
+            obsSpeed = 5;
+        }
+    }
+}
